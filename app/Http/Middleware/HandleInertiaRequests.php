bc313b892c550132c1c839cb5c3e2cc3811dfd90
ibigsarl <ibinter.group.adm@gmail.com>
2026-07-19 17:50:08 +0000

    fix: disable Inertia version check causing 409 navigation failure on LWS

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -22,7 +22,7 @@ class HandleInertiaRequests extends Middleware
      */
     public function version(Request $request): ?string
     {
-        return parent::version($request);
+        return null;
     }
 
     /**
